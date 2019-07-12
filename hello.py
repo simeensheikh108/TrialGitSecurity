3ed0602f27495cd1fa2182595919a55a77a3c50e
simeensheikh108 <33218419+simeensheikh108@users.noreply.github.com>
2019-07-12 04:08:32 -0400

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-client = boto3.client(
-    's3',
-    aws_access_key_id=AZIAW2G2QUWED8RCXB5R,
-    aws_secret_access_key=vRCK2fYVrXkHjq9KJYaCZ7rJMioMpSf9tq+DSK6+,
-    aws_session_token=SESSION_TOKEN,
-)
-
