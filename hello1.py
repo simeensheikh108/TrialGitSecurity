9130ed6274d6c73bafb36732ccc4a6e7a2c31b03
simeen <simeen.sheikh@broadridge.com>
2019-07-12 08:19:27 +0000

    hey

---
+++ hello1.py
@@ -0,0 +1,7 @@
+client = boto3.client(
+    's3',
+    aws_access_key_id=AZIAW2G2QUWED8RCXB5R,
+    aws_secret_access_key=vRCK2fYVrXkHjq9KJYaCZ7rJMioMpSf9tq+DSK6+,
+    aws_session_token=SESSION_TOKEN,
+)
+
